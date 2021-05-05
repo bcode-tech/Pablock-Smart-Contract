30acdca2f6dd1cc93e02c328523a3b08d2c8e9f5
Riccardo Grespan <riccardo.grespan@knobs.it>
2021-05-05 18:36:48 +0200

    UPDATE: remove GSN contract

--- contracts/GSN/ConvertLib.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
-
-library ConvertLib{
-    function convert(uint amount,uint conversionRate) internal pure returns (uint convertedAmount)
-    {
-        return amount * conversionRate;
-    }
-}
