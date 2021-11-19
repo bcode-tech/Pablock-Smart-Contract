736239edb11b817e491aee0fcf08c038f68425fd
Riccardo Grespan <riccardo.grespan@knobs.it>
2021-11-19 12:18:03 +0100

    UPDATE: code refactor

--- scripts/pablockToken.js
+++ scripts/pablockToken.js
@@ -1,8 +1,10 @@
+const currentEnv = process.env.ENV || "LOCAL";
+
 const fs = require("fs");
 const { ethers } = require("ethers");
 
 const infuraKey = fs.readFileSync(".infurakey.secret").toString().trim();
-const maticMnemonic = fs.readFileSync(".mumbai.secret").toString().trim();
+const privateKey = fs.readFileSync(`.${currentEnv}.secret`).toString().trim();
 
 const PablockTokenJSON = require("../build/contracts/PablockToken.json");
 
@@ -12,17 +14,12 @@ const provider = new ethers.providers.JsonRpcProvider(
 );
 
 // const contractOwner = new ethers.Wallet(maticMnemonic);
-const contractOwner = new ethers.Wallet(
-  // "0x52f6882af2362a0f3b9efc67e2521b6a698283e88ebb5428285168ea303fd15b", //LOCAL
-  "0xe8bf741fada50a9a5d156631c5201c6d2c5dd38e168d246ea3cf1d313d9101bb" //MUMBAI
-);
+const contractOwner = new ethers.Wallet(privateKey);
 
 const contractAddress = "0x22D1389a75F6B038ea076125B8774545aC697551";
 
 const pablockToken = new ethers.Contract(
   "0xa723fc1E105923a98a7FbdB8040296C8f118d883", //Mumbai
-  // "0x2b9233683001657161db866c7405493Fc1d1C22d", //Local Legacy
-  // "0x2F73D51b8813775D8CFB2a7147b516CB01EEb4C2", //LOCAL Permit enabled
   PablockTokenJSON.abi,
   contractOwner.connect(provider)
 );
